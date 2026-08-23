262f4ef23072cdf88885fce2f7020c31b9c26101
Ketsebaot Gizachew <ketsebaot.vibes@gmail.com>
2026-08-23 20:29:55 +0300

    remove build

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Xyla_FILE /home/k/projects/xyla/build/Xyla)
-set(__QT_DEPLOY_TARGET_Xyla_TYPE EXECUTABLE)
--- build/.qt/Xyla_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "Xyla")
-set(working_dir "/home/k/projects/xyla")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/k/projects/xyla/build/.qt/Xyla_res.txt")
