1c59b38fdee6fbb332b6011c8286340d1c6de4dd
x <x>
2026-09-18 00:25:24 +0300

    linted src/core/audio/hal/pipewireAudioBackend.hpp

--- src/core/audio/hal/pipewireAudioBackend.hpp
+++ src/core/audio/hal/pipewireAudioBackend.hpp
@@ -5,7 +5,8 @@
 #include <memory>
 #include <thread>
 
-// Forward declare PipeWire & SPA opaque structures to avoid polluting external headers
+// Forward declare PipeWire & SPA opaque structures to avoid polluting external
+// headers
 struct pw_main_loop;
 struct pw_stream;
 struct spa_hook;
@@ -14,46 +15,46 @@ namespace xyla::audio {
 
 class PipeWireAudioBackend final : public IAudioDeviceBackend {
 public:
-    PipeWireAudioBackend();
-    ~PipeWireAudioBackend() override;
+  PipeWireAudioBackend();
+  ~PipeWireAudioBackend() override;
 
-    PipeWireAudioBackend(const PipeWireAudioBackend &) = delete;
-    PipeWireAudioBackend &operator=(const PipeWireAudioBackend &) = delete;
+  PipeWireAudioBackend(const PipeWireAudioBackend &) = delete;
+  PipeWireAudioBackend &operator=(const PipeWireAudioBackend &) = delete;
 
-    [[nodiscard]] const char *backendName() const noexcept override {
-        return "PipeWire";
-    }
+  [[nodiscard]] const char *backendName() const noexcept override {
+    return "PipeWire";
+  }
 
-    bool initialize(const AudioDeviceConfig &config,
-                    IAudioRenderCallback *callback) override;
-    bool start() override;
-    bool stop() override;
-    void shutdown() override;
+  bool initialize(const AudioDeviceConfig &config,
+                  IAudioRenderCallback *callback) override;
+  bool start() override;
+  bool stop() override;
+  void shutdown() override;
 
-    [[nodiscard]] bool isRunning() const noexcept override {
-        return m_running.load(std::memory_order_acquire);
-    }
+  [[nodiscard]] bool isRunning() const noexcept override {
+    return m_running.load(std::memory_order_acquire);
+  }
 
-    [[nodiscard]] AudioDeviceConfig currentConfig() const noexcept override {
-        return m_config;
-    }
+  [[nodiscard]] AudioDeviceConfig currentConfig() const noexcept override {
+    return m_config;
+  }
 
-    // Called on PipeWire RT stream process callback
-    void onProcess();
+  // Called on PipeWire RT stream process callback
+  void onProcess();
 
 private:
-    AudioDeviceConfig m_config;
-    IAudioRenderCallback *m_callback{nullptr};
+  AudioDeviceConfig m_config;
+  IAudioRenderCallback *m_callback{nullptr};
 
-    struct pw_main_loop *m_loop{nullptr};
-    struct pw_stream *m_stream{nullptr};
-    std::unique_ptr<struct spa_hook> m_streamListener;
+  struct pw_main_loop *m_loop{nullptr};
+  struct pw_stream *m_stream{nullptr};
+  std::unique_ptr<struct spa_hook> m_streamListener;
 
-    std::thread m_loopThread;
-    std::atomic<bool> m_running{false};
-    AudioBuffer m_pipewireBuffer;
+  std::thread m_loopThread;
+  std::atomic<bool> m_running{false};
+  AudioBuffer m_pipewireBuffer;
 
-    int64_t m_hardwareSampleCounter{0};
+  int64_t m_hardwareSampleCounter{0};
 };
 
 } // namespace xyla::audio
