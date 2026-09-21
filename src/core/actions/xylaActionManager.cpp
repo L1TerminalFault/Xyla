6b33dc40c0737b273ac0385f639815e42b68d963
x <x>
2026-09-21 22:23:12 +0300

    action manager clean up

--- src/core/actions/xylaActionManager.cpp
+++ src/core/actions/xylaActionManager.cpp
@@ -164,9 +164,9 @@ bool XylaActionManager::triggerAction(const QString &actionId) {
     }
   }
 
-  XYLA_LOG_INFO("XylaActionManager", "Action triggered [" +
-                                         currentDockPrefix().toStdString() +
-                                         "]: " + resolved.toStdString());
+  // XYLA_LOG_INFO("XylaActionManager", "Action triggered [" +
+  //                                        currentDockPrefix().toStdString() +
+  //                                        "]: " + resolved.toStdString());
 
   if (it->callback) {
     it->callback();
